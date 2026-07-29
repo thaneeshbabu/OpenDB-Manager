08dbb945883694d67e33ba3b98068ae48fbb1fb9
thaneeshbabu <lingamthaneesh782@gmail.com>
2026-07-30 00:23:05 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,15 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://opendb-manager-backend.onrender.com/api",
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://opendb-manager-backend.onrender.com/api",
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default api;
